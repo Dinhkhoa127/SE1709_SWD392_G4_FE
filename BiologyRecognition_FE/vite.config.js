4c48ab6804e48d2f190fc6cb7d0eca9325e0f1ac
Dinhkhoa127 <dinhkhoa127@gmail.com>
2025-06-12 09:51:02 +0700

    Set up for deploy into Vercel

--- BiologyRecognition_FE/vite.config.js
+++ BiologyRecognition_FE/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/SE1709_SWD392_G4_FE',
 })
 
